e6e2347e7cad55fb69081d38ad4e80b2e0a71608
1218AND <1218AND@6A-12>
2014-02-14 09:30:57 +0900

    0.0.00(Alpha)
    クラスの構成やレイアウトの構成を見直すためプロジェクトの新規作成

---
+++ src/com/example/whatif/Deck.java
@@ -0,0 +1,12 @@
+package com.example.whatif;
+
+import java.util.ArrayList;
+
+
+public class Deck {
+	ArrayList<Trump> deck;
+
+	public Deck() {
+		deck = new ArrayList<Trump>();
+	}
+}
---
+++ src/com/example/whatif/Trump.java
@@ -0,0 +1,16 @@
+package com.example.whatif;
+
+public class Trump {
+	
+	//トランプの数字(1～13)
+	private int number; 
+	//トランプの図柄(1234:????)
+	private int suit;
+
+	public Trump(int number, int suit) {
+		this.number = number;
+		this.suit = suit;
+	}
+
+}
+
