62f7789e77bc27dfc3e7b64ed5baf49d85e472c6
1218AND <1218AND@6A-12>
2014-02-22 14:41:04 +0900

    0.0.07(Alpha)
    Y軸回転の処理を移植中

--- src/com/example/whatif/WhatifActivity.java
+++ src/com/example/whatif/WhatifActivity.java
@@ -1,43 +1,56 @@
 package com.example.whatif;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
 import android.widget.FrameLayout;
 
 public class WhatifActivity extends Activity {
-	private TrumpView trumpView;
-	private TrumpView layoutView;
-	private TrumpView clearView0;
-	private TrumpView clearView1;
-	private TrumpView clearView2;
-	private TrumpView clearView3;
-	private TrumpView clearView4;
-	private TrumpView clearView5;
+
+	private TrumpView[] clearView = new TrumpView[6];
+	private TrumpView[] trumpView = new TrumpView[6];
+	private TrumpView trumpBackView;
 
 	private FrameLayout mainFrame;
 
-	private FrameLayout layoutFrame;
-	private FrameLayout hand1Frame;
-	private FrameLayout hand2Frame;
-	private FrameLayout hand3Frame;
-	private FrameLayout hand4Frame;
-	private FrameLayout hand5Frame;
+	private FrameLayout[] layoutFrame = new FrameLayout[6];
+
+	private int[] frameId = { R.id.layoutLocation,
+			R.id.Hand1Field,
+			R.id.Hand2Field,
+			R.id.Hand3Field,
+			R.id.Hand4Field,
+			R.id.Hand5Field };
 
 	private CheckDevice cd;
 	private Deck deck;
 
-	private int[] layoutView_location = new int[2];
-	private int StatusBarHeight;
+	private int statusbarHeight;
+
+	private ArrayList<Point> clearView_location = new ArrayList<Point>();
+	private int[] layout_location = new int[2];
+	//	private ArrayList<Point> trumpView_location = new ArrayList<Point>();
+
+	private boolean flag = true;// 表と裏のどちらが表示されているか判断
+	private boolean animFlag = true;// アニメーション中はfalseとなり画像をクリックできない
+	private float centerX;// Y軸回転の中心点(X座標)を設定
+	private float centerY;// Y軸回転の中心点(Y座標)を設定
+	private long time = 500;// Y軸回転のアニメーションスピード
 
 	// LogCat用のタグを定数で定義する
 	public static final String TAG = "Test";
-	
+
 	/* ********** ********** ********** ********** */
 
 	@Override
@@ -56,61 +69,35 @@ public class WhatifActivity extends Activity {
 		// 画像配置用に最前面のレイアウトのインスタンスを取得
 		mainFrame = (FrameLayout) findViewById(R.id.FrameLayout);
 
-		// トランプ画像のインスタンスを取得
-		trumpView = new TrumpView(this.getApplicationContext());
-		trumpView = (TrumpView) trumpView.addLayoutView(this.getApplicationContext());
+		for (int i = 0; i < 6; i++) {
+			// 場札と手札1～5を配置するレイアウトのインスタンスを取得
+			layoutFrame[i] = (FrameLayout) findViewById(frameId[i]);
 
-		// 場札のインスタンスを取得
-		clearView0 = new TrumpView(this.getApplicationContext());
-		clearView0 = (TrumpView) clearView0.addClearView(this.getApplicationContext());
+			// 場札と手札1～5のViewインスタンスを取得
+			clearView[i] = new TrumpView(this.getApplicationContext());
+			clearView[i] = (TrumpView) clearView[i].addClearView(this.getApplicationContext());
 
-		// 場札を配置するレイアウトのインスタンスを取得
-		layoutFrame = (FrameLayout) findViewById(R.id.layoutLocation);
+			// FrameLayoutにトランプ画像を配置するため、画像の幅・高さ・重力を設定しておく
+			// このパラメーターは手札1～5にも使用する
+			FrameLayout.LayoutParams params =
+					new FrameLayout.LayoutParams(clearView[i].getTrumpWidth(), clearView[i].getTrumpHeight(), Gravity.CENTER);
 
-		// FrameLayoutにトランプ画像を配置するため、画像の幅・高さ・重力を設定しておく
-		// このパラメーターは手札1～5にも使用する
-		FrameLayout.LayoutParams params =
-				new FrameLayout.LayoutParams(trumpView.getTrumpWidth(), trumpView.getTrumpHeight(), Gravity.CENTER);
+			// レイアウトに場札と手札1～5の画像を配置する
+			layoutFrame[i].addView(clearView[i], params);
+		}
 
-		// レイアウトに場札画像を一時的に配置する
-		layoutFrame.addView(clearView0, params);
+		trumpView[0] = new TrumpView(this.getApplicationContext());
+		trumpView[0] = (TrumpView) trumpView[0].addLayoutView(this.getApplicationContext());
 
-		// 手札1～5を配置するレイアウトのインスタンスを取得
-		hand1Frame = (FrameLayout) findViewById(R.id.Hand1Field);
-		hand2Frame = (FrameLayout) findViewById(R.id.Hand2Field);
-		hand3Frame = (FrameLayout) findViewById(R.id.Hand3Field);
-		hand4Frame = (FrameLayout) findViewById(R.id.Hand4Field);
-		hand5Frame = (FrameLayout) findViewById(R.id.Hand5Field);
+		trumpBackView = new TrumpView(this.getApplicationContext());
+		trumpBackView = (TrumpView) trumpBackView.addBackView(this.getApplicationContext());
 
-		// 手札1～5の配置座標を計測するためのビューの取得
-		clearView1 = new TrumpView(this.getApplicationContext());
-		clearView1 = (TrumpView) clearView1.addClearView(this.getApplicationContext());
+		centerX = trumpBackView.getTrumpWidth() / 2;
+		centerY = trumpBackView.getTrumpHeight();
 
-		clearView2 = new TrumpView(this.getApplicationContext());
-		clearView2 = (TrumpView) clearView2.addClearView(this.getApplicationContext());
-
-		clearView3 = new TrumpView(this.getApplicationContext());
-		clearView3 = (TrumpView) clearView3.addClearView(this.getApplicationContext());
-
-		clearView4 = new TrumpView(this.getApplicationContext());
-		clearView4 = (TrumpView) clearView4.addClearView(this.getApplicationContext());
-
-		clearView5 = new TrumpView(this.getApplicationContext());
-//		clearView5 = (TrumpView) clearView5.addClearView(this.getApplicationContext());
-		clearView5 = (TrumpView) clearView5.addBackView(this.getApplicationContext());
-
-		
-		// レイアウトに手札1～5の画像を配置する
-		hand1Frame.addView(clearView1, params);
-		hand2Frame.addView(clearView2, params);
-		hand3Frame.addView(clearView3, params);
-		hand4Frame.addView(clearView4, params);
-		hand5Frame.addView(clearView5, params);
-
-		//		トランプの裏面作成
-		//		トランプが配られたときの演出（上から下へ表示される）
 		//		トランプが裏返る表現
 		//		トランプが移動するアニメ
+
 		Log.v(TAG, "onCreate()");
 	}// onCreate()
 
@@ -121,40 +108,35 @@ public class WhatifActivity extends Activity {
 		// ステータスバーの高さを取得
 		Rect rect = new Rect();
 		getWindow().getDecorView().getWindowVisibleDisplayFrame(rect);
-		StatusBarHeight = rect.top;
-		Log.v(TAG, "ステータスバー height=" + StatusBarHeight);
+		statusbarHeight = rect.top;
+		Log.v(TAG, "ステータスバー height=" + statusbarHeight);
+		setClearViewPoint();
 
-		int[] clearView0_location = new int[2];
-		clearView0.getLocationInWindow(clearView0_location);
-		int[] clearView3_location = new int[2];
-		clearView3.getLocationInWindow(clearView3_location);
-
-		layoutView_location[0] = Math.max(clearView0_location[0], clearView3_location[0]);
-		layoutView_location[1] = clearView0_location[1]-StatusBarHeight;
-
-//		Log.v(TAG, "0 x=" + clearView0_location[0] + " y=" + clearView0_location[1]);
-//		Log.v(TAG, "3 x=" + clearView3_location[0] + " y=" + clearView3_location[1]);
-//
-//		Log.v(TAG, "layout x=" + layoutView_location[0] + " y=" + layoutView_location[1]);
+		layout_location[0] = Math.max(clearView_location.get(0).x, clearView_location.get(3).x);
+		layout_location[1] = clearView_location.get(0).y - statusbarHeight;
 
 		// 画像配置用に最前面のレイアウトのインスタンスを取得
 		mainFrame = (FrameLayout) findViewById(R.id.FrameLayout);
 
 		FrameLayout.LayoutParams params =
-				new FrameLayout.LayoutParams(trumpView.getTrumpWidth(), trumpView.getTrumpHeight());
+				new FrameLayout.LayoutParams(trumpView[0].getTrumpWidth(), trumpView[0].getTrumpHeight());
 
-		layoutView = new TrumpView(this.getApplicationContext());
-		layoutView = (TrumpView) layoutView.addLayoutView(this.getApplicationContext());
-
-		params.leftMargin = layoutView_location[0];
-		params.topMargin = layoutView_location[1];
+		params.leftMargin = layout_location[0];
+		params.topMargin = layout_location[1];
 		params.gravity = Gravity.NO_GRAVITY;// この記載がないとマージンが有効にならない
 
 		// レイアウトに場札画像を配置する
-		mainFrame.addView(layoutView, params);
-		
+		mainFrame.addView(trumpView[0], params);
+		trumpView[0].setVisibility(View.INVISIBLE);
+
 		// レイアウトの一時配置用の画像を非表示にする(削除するとレイアウトが崩れるため非表示を選択)
-		clearView0.setVisibility(View.INVISIBLE);
+		clearView[0].setVisibility(View.INVISIBLE);
+
+		mainFrame.addView(trumpBackView, params);
+		// trumpBackView.setVisibility(View.INVISIBLE);
+
+		trumpView[0].setOnClickListener(layoutClick);// Y軸回転テスト用のクリックリスナー
+		trumpBackView.setOnClickListener(trumpBackViewClick);// Y軸回転テスト用のクリックリスナー
 
 		Log.v(TAG, "onWindowFocusChanged()");
 
@@ -206,5 +188,103 @@ public class WhatifActivity extends Activity {
 
 	/* ********** ********** ********** ********** */
 
+	public void FlipTrump(View v, int index) {
+
+		if (animFlag) {
+			// アニメーション中にクリックできないようfalseに変更する
+			animFlag = false;
+
+			// 現在表示されているトランプ画像をレイアウトから削除する
+			// mainFrame.removeView(v);
+
+			// Y軸回転(0～90度)
+			Rotate3dAnimation rotation = new Rotate3dAnimation(0, 90, centerX, centerY, 0f, true);
+			rotation.setDuration(time);
+			trumpBackView.startAnimation(rotation);
+			rotation.setAnimationListener(startFlip);
+			Log.v(TAG, "anime");
+		}
+	}
+
+	// http://kinsentansa.blogspot.jp/2012/04/android_13.html
+	
+	public AnimationListener startFlip = new AnimationListener() {
+
+		@Override
+		public void onAnimationStart(Animation animation) {
+		}
+
+		@Override
+		public void onAnimationRepeat(Animation animation) {
+		}
+
+		@Override
+		public void onAnimationEnd(Animation animation) {
+
+			trumpBackView.setVisibility(View.INVISIBLE);
+
+			// Y軸回転(270～360度)
+			Rotate3dAnimation rotation = new Rotate3dAnimation(270, 360, centerX, centerY, 0f, false);
+			rotation.setDuration(time);
+			trumpView[0].startAnimation(rotation);
+			rotation.setAnimationListener(endFlip);
+			Log.v(TAG, "startFlip");
+		}
+	};
+
+	public AnimationListener endFlip = new AnimationListener() {
+
+		@Override
+		public void onAnimationStart(Animation animation) {
+		}
+
+		@Override
+		public void onAnimationRepeat(Animation animation) {
+		}
+
+		@Override
+		public void onAnimationEnd(Animation animation) {
+			// アニメーションの終了
+			trumpView[0].setVisibility(View.VISIBLE);
+			animFlag = true;
+			Log.v(TAG, "endFlip");
+		}
+	};
+
+	// ArrayListにclearViewのXY座標を格納する
+	private void setClearViewPoint() {
+		int[] location = new int[2];
+		for (int i = 0; i < 6; i++) {
+
+			clearView[i].getLocationInWindow(location);
+			int x = location[0];
+			int y = location[1];
+			Point point = new Point(x, y);
+			clearView_location.add(point);
+
+		}
+	}
+
+	// ArrayListに格納したclearViewのXY座標を取得する
+
+	// レイアウトViewをクリックした時の処理
+	private OnClickListener layoutClick = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			Log.v(TAG, "layout...CLICK");
+			FlipTrump(v, 0);
+		}
+	};
+
+	// レイアウトViewをクリックした時の処理
+	private OnClickListener trumpBackViewClick = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			Log.v(TAG, "layout...CLICK");
+			FlipTrump(v, 0);
+		}
+	};
 
 }
